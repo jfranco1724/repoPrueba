f8c27828a00007064210f2dac92a6e69a3c273b9
jeisson <jfranco1705@gmail.com>
2021-03-23 12:54:36 -0500

    tird commit

--- Source.cpp
+++ Source.cpp
@@ -2,5 +2,10 @@
 using namespace std;
 int main() {
 	cout << "hello world" << endl;
+	cout << "texto1" << endl;
+	cout << "texto2" << endl;
+	cout << "texto3" << endl;
+	cout << "texto4" << endl;
+	cout << "texto5" << endl;
 	return 0;
 }
